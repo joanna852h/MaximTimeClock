fd9346c0197437ff6f82e298d93e0007b10d36a3
joanna852h <joanna852hawks@outlook.com>
2024-08-31 17:35:26 +0530

    Create src_components_Receipt.js

---
+++ src_components_Receipt.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Receipt.css';
+
+function Receipt() {
+  return (
+    <div className="receipt">
+      <h2>Receipt</h2>
+      <p>Total: $170</p>
+      <p>Thank you for using MaximTimeClock!</p>
+    </div>
+  );
+}
+
+export default Receipt;
